f64c8e5d0e5b630b1480a23e91310a5a51b9ef5d
mehanix <nixikunn@gmail.com>
2021-02-03 11:48:36 +0200

    work on 6

--- R/ex6.r
+++ R/ex6.r
@@ -1,10 +1,16 @@
 ### Cerinta 6: Calculul mediei și dispersiei unei variabile aleatoare g(X), unde X are o repartiție continuă cunoscută
 ## iar g este o funcție continuă precizată de utilizator.
 
-source("integrate.R")
-source("dispersion.R")
+medium <- function(f, d = c(-Inf, Inf)){
+  integrate(function(x){ x * f(x)}, d[1], d[2]) $ value
+}
+
 ex6 <- function(g, fx) {
   # y = g(X) e o noua variabila aleatoare, ii calculez media
   # folosesc formula pt media functiilor de x
-  e_y <- safe_integrate(function(x)(h(x) *fx(x)))
+  gx <- g(fx)
+  e_y <- integrate(function(x)(h(x) *fx(x)))
+  # pt dispersie, folosesc compunerea celor 2 functii si media calculata inainte
+  new_f<-(x-e_y)^2
+  dispersie <-medium(new_f)
 }
