4c066ed32ed2657b31fa360891b3545fed0bad50
Stefan Radu <stefan270101@gmail.com>
2021-02-03 11:47:41 +0200

    added normal and pareto

--- R/ex4.8.R
+++ R/ex4.8.R
@@ -2,43 +2,60 @@
 # folosind definitia
 get_CDF_from_PDF <- function (f, x) {
   tryCatch ({
-      integral(Vectorize(f), 0, x)
+      integral(Vectorize(f), -Inf, x, method="Gauss")
     }, error = function (e) {
       # print(e)
     }
   )
 }
 
-plot_density_or_repartition <- function(f, a, b) {
+check_density <- function(f, a, b) {
   # verific prima conditie
-  for (x in seq(a, b, 0.01)) {
+  for (x in seq(a, b, 0.1)) {
     if (f(x) < 0) {
       print(paste("pdf nu poate sa aiba valori negative",
                   "dar am obtinut", f(x), "in", x, sep=" "))
-      return()
+      return(FALSE)
     }
   }
 
   # verific a doua conditie
   total <- integral(Vectorize(f), max(-Inf, a), min(b, +Inf))
-  if (total != 1) {
+  if (abs(total - 1) > 0.1) {
     print(paste("probabilitatea cumulata totala trebuie sa fie egala cu 1",
                 "dar am obtinut", total, sep=" "))
+    return(FALSE)
+  }
+
+  return(TRUE)
+}
+
+plot_density <- function(f, a, b) {
+  # validez pdf
+  if (!check_density(f, a, b)) {
     return()
   }
 
   # pdf a fost validata
   # afisez graficul pdf
 
-  xs <- seq(a, b, 0.01)
+  xs <- seq(a, b, 0.1)
   ys <- c()
   for (x in xs) {
     ys = append(ys, f(x))
   }
   plot(xs, ys, type="l", main="PDF", col="red", xlab="x", ylab="y")
+}
+
+plot_repartition <- function(f, a, b) {
+  # validez pdf
+  if (!check_density(f, a, b)) {
+    return()
+  }
 
   # calculez si afisez graficul CDF
 
+  xs <- seq(a, b, 0.01)
   ys <- c()
   for (x in xs) {
     ys = append(ys, get_CDF_from_PDF(f, x))
@@ -46,7 +63,7 @@ plot_density_or_repartition <- function(f, a, b) {
   plot(xs, ys, col="red", type="l", main="CDF", xlab="x", ylab="y")
 }
 
-parse_known_repartition <- function(name, ...) {
+parse_known_repartition <- function(name, CDF=FALSE, ...) {
   params <- list(...)
   if (name == "uniform") {
     if (!is.null(params$a) && !is.null(params$b)) {
@@ -57,7 +74,12 @@ parse_known_repartition <- function(name, ...) {
       }
 
       f <- function(x) 1 / (b - a)
-      plot_density_or_repartition(f, a, b)
+      if (CDF) {
+        plot_repartition(f, a, b)
+      }
+      else {
+        plot_density(f, a, b)
+      }
     }
     else {
       return("parametrii necesari nu au fost pasati")
@@ -65,19 +87,67 @@ parse_known_repartition <- function(name, ...) {
   }
   else if (name == "exp") {
     if (!is.null(params$lambda)) {
-      lambda <- params$labmda
-      if (labmda <= 0) {
+      lambda <- params$lambda
+      if (lambda <= 0) {
         return("parametru invalizi")
       }
 
       f <- function(x) (lambda * exp(1)^(-lambda * x))
-      plot_density_or_repartition(f, 0, Inf)
+      if (CDF) {
+        plot_repartition(f, 0, 50)
+      }
+      else {
+        plot_density(f, 0, 50)
+      }
     }
     else {
       return("parametrii necesari nu au fost pasati")
     }
   }
+  else if (name == "normal") {
+    if (!is.null(params$mu && !is.null(params$sigma))) {
+      mu <- params$mu
+      sigma <- params$sigma
+      if (sigma <= 0) {
+        return("parametru invalid")
+      }
+
+      f <- function(x) ((1 / (sigma * sqrt(pi * 2)))*(exp(1)^((-(x - mu)^2)/(2  * sigma ^ 2))))
+      if (CDF) {
+        plot_repartition(f, -25, 25)
+      }
+      else {
+        plot_density(f, -25, 25)
+      }
+    }
+    else {
+      return("parametrii necesari nu au fost pasati")
+    }
+  }
+  else if (name == "pareto") {
+    if (!is.null(params$m) && !is.null(params$alpha)) {
+      m <- params$m
+      alpha <-params$alpha
+      if (alpha <= 0 || m <= 0) {
+        return("parametru invalid")
+      }
+
+      f <- function(x) (alpha * m^alpha) / (x ^ (alpha + 1))
+      if (CDF) {
+        plot_repartition(f, m, m + 50)
+      }
+      else {
+        plot_density(f, m, m + 50)
+      }
+    }
+  }
 }
 
-parse_known_repartition("uniform", a=0, b=10)
-parse_known_repartition("exp", lambda=2)
+#parse_known_repartition("uniform", FALSE, a=0, b=10)
+#parse_known_repartition("uniform", TRUE, a=0, b=10)
+#parse_known_repartition("exp", FALSE, lambda=2)
+#parse_known_repartition("exp", TRUE, lambda=2)
+#parse_known_repartition("normal", FALSE, mu=0, sigma=1)
+#parse_known_repartition("normal", TRUE, mu=0, sigma=1)
+#parse_known_repartition("pareto", FALSE, m=3, alpha=1)
+parse_known_repartition("pareto", TRUE, m=3, alpha=1)
